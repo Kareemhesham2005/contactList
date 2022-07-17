dd644e1b19c1abffafcf978e6bde05881b7d869c
Kareem Hesham <85392795+Kareemhesham2005@users.noreply.github.com>
2022-07-17 13:37:03 +0200

    undo test

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import { BrowserRouter } from "react-router-dom";
-
-// const root = ReactDOM.createRoot(document.getElementById("root")).render(
-//   // <BrowserRouter>
-//   <App />
-//   // </BrowserRouter>
-// );
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
