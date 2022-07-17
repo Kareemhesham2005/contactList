7926e99cdd2c886101f520a2b2142dd29f3711d8
Kareem Hesham <85392795+Kareemhesham2005@users.noreply.github.com>
2022-07-17 02:52:26 +0200

    Imported API

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import "./App.css";
 import ListContacts from "./ListContacts";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import * as ContactsAPI from "./utils/ContactsAPI";
 
 function App() {
   const removeContact = (contact) => {
     setContacts(contacts.filter((c) => contact.id !== c.id));
   };
-  const [contacts, setContacts] = useState([
-    {
-      id: "karen",
-      name: "Karen Isgrigg",
-      handle: "@karen_isgrigg",
-      avatarURL: "http://localhost:5001/karen.jpg",
-    },
-    {
-      id: "richard",
-      name: "Richard Kalehoff",
-      handle: "@richardkalehoff",
-      avatarURL: "http://localhost:5001/richard.jpg",
-    },
-    {
-      id: "tyler",
-      name: "Tyler McGinnis",
-      handle: "@tylermcginnis",
-      avatarURL: "http://localhost:5001/tyler.jpg",
-    },
-  ]);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const res = await ContactsAPI.getAll();
+      setContacts(res);
+    };
+
+    getContacts();
+  }, []);
+
   return (
     <div>
       <ListContacts contacts={contacts} onDeleteContact={removeContact} />
