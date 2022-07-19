e5d83ed0d99b09018a887c650546a2b1aef5d16a
Kareem Hesham <85392795+Kareemhesham2005@users.noreply.github.com>
2022-07-19 03:55:12 +0200

    Update ContactsAPI.js

--- src/utils/ContactsAPI.js
+++ src/utils/ContactsAPI.js
@@ -1,8 +1,8 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || "https://react-contact-server.herokuapp.com/";
+const api = process.env.REACT_APP_CONTACTS_API_URL || "http://localhost:5001/";
 
-let token = "kareem";
+let token = localStorage.token;
 
-if (!token) token = localStorage.token = "kareem";
+if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
 
 const headers = {
   Accept: "application/json",
